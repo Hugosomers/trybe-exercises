34d925040180436e35179ec37ee7ee6964af02d4
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-03-24 18:50:18 -0300

    Feito exercício 3

---
+++ exercises/bloco_4/dia_1/exercise_3.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = 5;
+let c = 3;
+
+if(a > b && a > c){
+  console.log("O a é maior");
+}else if(b > a && b > c){
+  console.log("O b é maior");
+}else if (c > a && c > b){
+  console.log("O c é maior");
+}else{
+  console.log("Eles são iguais");
+}
