83b13d3dfd082436bdd4d60edda3f04d52062e1e
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-03-25 19:34:02 -0300

    Feito o exercício 9

---
+++ exercises/bloco_4/dia_2/exercicio_9.js
@@ -0,0 +1,16 @@
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let longArray = [];
+// let division;
+for(let index = 1; index<=25; index += 1) {
+  longArray.push(index/2);
+}
+console.log(longArray);
+
+// Segunda maneira
+
+// for(let indexD = 0; indexD < longArray.length; indexD += 1){
+//   division = longArray[indexD]/2;
+//   console.log(division);
+// }
+
