89f61b78f88ab61816ef54442f4ff59e94f02929
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-03-26 18:25:51 -0300

    Feito exercício 2

--- exercises/bloco_4/dia_3/exercicio_1.js
+++ exercises/bloco_4/dia_3/exercicio_1.js
@@ -1,5 +1,10 @@
-let n = 10;
+let n = -1;
 let strg = '*';
 for(let index = 1; index <= n; index += 1 ){
-  console.log(strg.repeat(n));
+  if (n > 1) {
+    console.log(strg.repeat(n));
+  }else{
+    console.log('O valor de N tem que ser maior que 1');
+    break;
+  }
 }
