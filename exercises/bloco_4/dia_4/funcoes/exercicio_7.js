a1971918dc881bd40d192f3921243dbcf9594701
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-03-29 21:17:24 -0300

    Feito o exercício 5 de funções

---
+++ exercises/bloco_4/dia_4/funcoes/exercicio_7.js
@@ -0,0 +1,26 @@
+function repeaterNumber(numbers){
+let cont;
+let storageCountRepeat = 0;
+let theNumber;
+  for(let i = 0; i < numbers.length; i+=1){
+    cont = 1;
+
+    for(let j = i+1; j < numbers.length; j+=1){
+      if(numbers[i] === numbers[j]){
+        cont += 1;
+      }
+    }
+
+    if(cont > storageCountRepeat){
+      storageCountRepeat = cont;
+      theNumber = numbers[i];
+    }
+
+  }
+return theNumber
+
+}
+
+let numbersArray = [2, 5, 2, 5, 8, 2, 5, 5, 10, 5];
+
+console.log(repeaterNumber(numbersArray));
