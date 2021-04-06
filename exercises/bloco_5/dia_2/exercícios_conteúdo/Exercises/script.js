031dff9f84906597201f156bc5826315fd77fc14
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-06 20:42:46 -0300

    Exercício 8 do bloco 5.2

--- exercises/bloco_5/dia_2/exercícios_conteúdo/Exercises/script.js
+++ exercises/bloco_5/dia_2/exercícios_conteúdo/Exercises/script.js
@@ -36,3 +36,44 @@ element = createElement('img');
 element.className = 'small-image';
 element.src = 'https://picsum.photos/200'
 document.querySelector('.left-content').appendChild(element);
+
+// Exercício 8
+let element3;
+let size = 10;
+for (let index = 1; index <= size; index += 1) {
+  element3 = createElement('li');
+  switch (index) {
+    case 1:
+      element3.innerText = 'um';
+      break;
+    case 2:
+      element3.innerText = 'dois';
+      break;
+    case 3:
+      element3.innerText = 'três';
+      break;
+    case 4:
+      element3.innerText = 'quatro';
+      break;
+    case 5:
+      element3.innerText = 'cinco';
+      break;
+    case 6:
+      element3.innerText = 'seis';
+      break;
+    case 7:
+      element3.innerText = 'sete';
+      break;
+    case 8:
+      element3.innerText = 'oito';
+      break;
+    case 9:
+      element3.innerText = 'nove';
+      break;
+    case 10:
+      element3.innerText = 'dez';
+      break;
+
+  }
+  document.querySelector('.right-content').appendChild(element3);
+}
