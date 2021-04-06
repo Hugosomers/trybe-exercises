1ae73db1b9623ceec40f6f83dd6900cc5aa6009c
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-06 15:30:08 -0300

    Exercício 3 do conteúdo parte I

--- exercises/bloco_5/dia_2/exercícios_conteúdo/script.js
+++ exercises/bloco_5/dia_2/exercícios_conteúdo/script.js
@@ -1,4 +1,5 @@
 console.log(document.querySelector('#elementoOndeVoceEsta'));
 document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'red';
+document.querySelector('#primeiroFilhoDoFilho').innerText = 'Adicione um texto a ele.';
 
 
