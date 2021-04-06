6b3d7768f9cd356fd1000130ce092babd269127a
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-06 15:38:24 -0300

    Exercícios finais do conteúdo parte I

--- exercises/bloco_5/dia_2/exercícios_conteúdo/script.js
+++ exercises/bloco_5/dia_2/exercícios_conteúdo/script.js
@@ -1,5 +1,9 @@
 console.log(document.querySelector('#elementoOndeVoceEsta'));
 document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = 'red';
 document.querySelector('#primeiroFilhoDoFilho').innerText = 'Adicione um texto a ele.';
-
+console.log(document.querySelector('#pai').firstElementChild);
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.firstElementChild);
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.childNodes[4]);
+console.log(document.querySelector('#elementoOndeVoceEsta').parentNode.childNodes[5]);
+console.log(document.querySelector('#pai').children[2]);
 
