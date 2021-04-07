01e37758e30009b14e29dcd67cf9d14bf5ed9600
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-07 20:11:33 -0300

    Exercício 1 do bloco 5.3 feito

--- exercises/bloco_5/dia_3/exercises/script.js
+++ exercises/bloco_5/dia_3/exercises/script.js
@@ -13,3 +13,26 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
+function createDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysUlList = document.querySelector('#days');
+  
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const singleDay = dezDaysList[index];
+    const daysUlListItems = document.createElement('li');
+    daysUlListItems.classList.add('day');
+    daysUlListItems.innerText = singleDay;
+    if (singleDay === 24 ||singleDay === 25 || singleDay === 31) {
+      daysUlListItems.classList.add('holiday');
+    }
+    if (singleDay === 4 || singleDay === 11 || singleDay === 18 || singleDay === 25) {
+      daysUlListItems.classList.add('friday');
+    }
+    daysUlList.appendChild(daysUlListItems);
+  }
+
+  
+
+}
+
+createDays();
