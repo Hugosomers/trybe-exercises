2fb964bdfe9604c31c774717c371a7b221967ed7
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-11 18:05:12 -0300

    Requisito 1 feito

--- exercises/bloco_5/dia_4/script.js
+++ exercises/bloco_5/dia_4/script.js
@@ -1,2 +1,17 @@
+// Requisito 1
+const nightBtn = document.querySelector('.night-button');
+const lightBtn = document.querySelector('.light-button');
 
+document.body.style.backgroundColor = localStorage.getItem('backgroundColor');
+
+
+nightBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'black';
+    localStorage.setItem('backgroundColor', 'black');
+});
+
+lightBtn.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white';
+    localStorage.setItem('backgroundColor', 'white');
+});
 
