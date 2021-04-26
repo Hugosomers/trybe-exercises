caedbe597b9f993e5f0f30f67b36ad14b57dd3e7
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-26 18:39:27 -0300

    Feito exercício 2 e bônus da parte I

--- exercises/bloco_7/dia_1/Parte_I/script.js
+++ exercises/bloco_7/dia_1/Parte_I/script.js
@@ -13,3 +13,33 @@ const testingScope = (escopo) => {
 }
 
 testingScope(false);
+
+// Exercício 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const orderedArray = (array) => {
+
+  for (let index = 0; index < array.length; index += 1) {
+    for (let j = index + 1; j < array.length; j += 1) {
+      if (array[index] > array[j]) {
+        let arrayPosition = array[index];
+        array[index] = array[j];
+        array[j] = arrayPosition;
+      }
+    }
+  }
+  return array;
+}
+
+
+
+console.log(`Os números ${orderedArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+
+// Exercício 2 - Bônus
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const comparedNumbers = (a, b) => a - b;
+
+console.log(`Os números ${oddsAndEvens.sort(comparedNumbers)} se encontram ordenados de forma crescente!`);
