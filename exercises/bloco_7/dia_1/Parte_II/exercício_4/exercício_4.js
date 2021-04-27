0c7406f2aa5c83c91bf44acf5e881304c6a166f5
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-26 21:19:33 -0300

    Feito exercício 4.2 da parte II

--- exercises/bloco_7/dia_1/Parte_II/exercício_4/exercício_4.js
+++ exercises/bloco_7/dia_1/Parte_II/exercício_4/exercício_4.js
@@ -4,4 +4,17 @@ const changeString = (string) => {
   return stringSave;
 }
 
-console.log(changeString('Bebeto'));
+// const returnFunction1 = ;
+// console.log(returnFunction1);
+
+const skills = ['HTML5', 'CSS3', 'JavaScript', 'ES6', 'BootStrap'];
+
+const mixAll = (returnFunction1) => {
+  const sortedSkills = skills.sort();
+  const lastWords = `Minhas cinco principais habilidades são:`;
+  const everything = `${changeString('Hugo')} ${lastWords} ${sortedSkills.toString()}`;
+  return everything;
+  
+}
+
+console.log(mixAll());
