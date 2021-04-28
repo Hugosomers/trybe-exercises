23fe2c3284f2397565f1d5794b7ce5a39a8b7304
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-28 20:13:28 -0300

    Exercício 1 parte I, completado

--- exercises/bloco_7/dia_3/conteúdo/script.js
+++ exercises/bloco_7/dia_3/conteúdo/script.js
@@ -63,9 +63,9 @@ function division(x, y) {
   // for verdadeira, o que irá interromper a execução da função.
 
   if (y === 0) throw new Error('parameter y must not be 0');
-  
+
   return x / y;
 }
 
-assert.strictEqual(division(10, 0), 5); // OK
+// assert.strictEqual(division(10, 0), 5); // OK
 assert.throws(() => { division(10, 0); }, /^Error: parameter y must not be 0$/); // OK
