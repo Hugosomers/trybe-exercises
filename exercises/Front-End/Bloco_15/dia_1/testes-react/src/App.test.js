df79ae757284f374a31cbfc5b9aafdb930c06145
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-06-21 19:15:34 -0300

    Conteúdo

---
+++ exercises/Front-End/Bloco_15/dia_1/testes-react/src/App.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
+
+// test('Verificando se existe o campo Email.', () => {
+//   const { getByLabelText } = render(<App />);
+//   const inputEmail = getByLabelText('Email');
+//   expect(inputEmail).toBeInTheDocument();
+//   expect(inputEmail.type).toBe('email');
+// });
+
+// test('Verificando se existe um botão', () => {
+//   const { getByRole } = render(<App />);
+//   const btn = getByRole('button');
+//   expect(btn).toBeInTheDocument();
+// });
+
+// test('Verificando se existe dois botões', () => {
+//   const { getAllByRole } = render(<App />);
+//   const buttons = getAllByRole('button');
+//   expect(buttons.length).toBe(2);
+// });
+
+// test('Verifica se existe um botão Enviar', () => {
+//   const { getByTestId }  = render(<App />);
+//   const btnId = getByTestId('id-send');
+
+//   expect(btnId).toBeInTheDocument();
+//   expect(btnId.type).toBe('button');
+//   expect(btnId).toHaveValue('Enviar');
+// })
+
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  const { getByTestId, getByLabelText } = render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = getByTestId('id-send');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+  fireEvent.click(btnSend);
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+});
