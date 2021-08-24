adca68fd875fd66e235cff67ff9783978683e396
Hugosomers <hugosomersla@gmail.com>
2021-08-24 18:36:31 -0300

    Exercícios do dia 5 ao 10

--- exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios do dia.sql
+++ exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios do dia.sql
@@ -11,11 +11,37 @@ ORDER BY Média DESC;
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
 SELECT SUM(SALARY) FROM hr.employees;
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
+SELECT ROUND(MAX(SALARY),2), ROUND(MIN(SALARY),2), ROUND(SUM(SALARY),2), ROUND(AVG(SALARY),2)
+FROM hr.employees;
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
+SELECT COUNT(*)
+FROM hr.employees
+WHERE JOB_ID = 'IT_PROG';
+
+SELECT JOB_ID, COUNT(*)
+FROM hr.employees
+GROUP BY JOB_ID
+HAVING JOB_ID = 'IT_PROG';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
+SELECT JOB_ID, SUM(SALARY) AS Soma
+FROM hr.employees
+GROUP BY JOB_ID;
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
+SELECT JOB_ID, SUM(SALARY) AS Soma
+FROM hr.employees
+GROUP BY JOB_ID
+HAVING JOB_ID = 'IT_PROG';
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( IT_PROG ).
--- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id .
+SELECT JOB_ID, AVG(SALARY) AS Média
+FROM hr.employees
+GROUP BY JOB_ID
+HAVING JOB_ID != 'IT_PROG'
+ORDER BY Média DESC;
+-- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id.
+SELECT DEPARTMENT_ID, AVG(SALARY) AS Média, COUNT(*) AS Funcs
+FROM hr.employees
+GROUP BY DEPARTMENT_ID
+HAVING Funcs > 10;
 -- 11. Escreva uma query que atualize a coluna PHONE_NUMBER , de modo que todos os telefones iniciados por 515 agora devem iniciar com 777 .
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id , primeiro nome e ano no qual foi contratado (exiba somente o ano).
