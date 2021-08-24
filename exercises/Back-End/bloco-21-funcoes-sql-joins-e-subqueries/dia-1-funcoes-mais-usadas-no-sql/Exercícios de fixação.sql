1d6f7445cb8a59bd51152ad582d5edd7ef68029f
Hugosomers <hugosomersla@gmail.com>
2021-08-24 17:27:13 -0300

    Done Exercícios de fixação Datas

--- exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios de fixação.sql
+++ exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios de fixação.sql
@@ -54,3 +54,11 @@ SELECT FLOOR(39.494);
 SELECT CEIL(85.234);
  
 -- -------------------------------------------------------------------------------
+-- Trabalhando com datas
+
+-- 1 - Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje.
+SELECT DATEDIFF('2030-01-20', CURRENT_DATE());
+-- 2 - Monte uma query exiba a diferença de horas entre '10:25:45' e '11:00:00'.
+SELECT TIMEDIFF('10:25:45', '11:00:00');
+
+-- -------------------------------------------------------------------------------
