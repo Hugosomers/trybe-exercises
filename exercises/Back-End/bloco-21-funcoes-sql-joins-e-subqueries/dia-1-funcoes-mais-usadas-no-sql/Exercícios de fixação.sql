474ec612a6b5ed91e4cae690652f608cb981795d
Hugosomers <hugosomersla@gmail.com>
2021-08-24 17:07:50 -0300

    Done Exercícios de fixação Condicionais

--- exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios de fixação.sql
+++ exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios de fixação.sql
@@ -1,6 +1,8 @@
 -- FIXAÇÃO
+
 -- -------------------------------------------------------------------------------
 -- Manipulação de strings
+
 -- 1 Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.
 SELECT UCASE('trybe');
 -- 2 Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?'.
@@ -11,4 +13,29 @@ SELECT LENGTH('Uma frase qualquer');
 SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas',13,10);
 -- 5 Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
 SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
+
+-- -------------------------------------------------------------------------------
+-- Condicionais
+-- 1 - Usando o IF na tabela sakila.film, exiba o id do filme, o título e uma coluna extra chamada 'conheço o filme?', em que deve-se avaliar se o nome do filme é ' ACE GOLDFINGER '. 
+-- Caso seja, exiba "Já assisti a esse filme". Caso contrário, exiba "Não conheço o filme". Não esqueça de usar um alias para renomear a coluna da condicional.
+SELECT film_id, title, IF(title = 'ACE GOLDFINGER', 'Já assisti a esse filme', 'Não conheço o filme') AS 'Conheço o filme?'
+FROM sakila.film;
+
+-- 2 - Usando o CASE na tabela sakila.film, exiba o título, a classificação indicativa ( rating ) e uma coluna extra que vamos chamar de 'público-alvo', 
+-- em que classificaremos o filme de acordo com as seguintes siglas: 
+-- G: "Livre para todos";
+-- PG: "Não recomendado para menores de 10 anos";
+-- PG-13: "Não recomendado para menores de 13 anos";
+-- R: "Não recomendado para menores de 17 anos";
+-- Se não cair em nenhuma das classificações anteriores: "Proibido para menores de idade".
+SELECT title, rating, 
+CASE
+	WHEN rating = 'G' THEN 'Livre para todos'
+    WHEN rating = 'PG' THEN 'Não recomendado para menores de 10 anos'
+    WHEN rating = 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+    WHEN rating = 'R' THEN 'Não recomendado para menores de 17 anos'
+ELSE 'Proibido para menores de idade'
+END AS 'público-alvo' 
+FROM sakila.film;
+
 -- -------------------------------------------------------------------------------
