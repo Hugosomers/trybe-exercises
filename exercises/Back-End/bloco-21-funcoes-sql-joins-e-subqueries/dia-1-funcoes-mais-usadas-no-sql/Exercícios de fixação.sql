ff39b2ea68df9e17fff1a7a4d38cb950c2e4f43d
Hugosomers <hugosomersla@gmail.com>
2021-08-24 17:34:04 -0300

    Done Exercícios de fixação Agregações

--- exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios de fixação.sql
+++ exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-1-funcoes-mais-usadas-no-sql/Exercícios de fixação.sql
@@ -62,3 +62,15 @@ SELECT DATEDIFF('2030-01-20', CURRENT_DATE());
 SELECT TIMEDIFF('10:25:45', '11:00:00');
 
 -- -------------------------------------------------------------------------------
+-- Funções de agregação
+
+-- 1 - A média de duração dos filmes e dê o nome da coluna de 'Média de Duração';
+SELECT AVG(length) AS 'Média de Duração' FROM sakila.film;
+-- 2 - A duração mínima dos filmes como 'Duração Mínima';
+SELECT MIN(length) AS 'Duração Mínima' FROM sakila.film;
+-- 3 - A duração máxima dos filmes como 'Duração Máxima';
+SELECT MAX(length) AS 'Duração Máxima' FROM sakila.film;
+-- 4 - A soma de todas as durações como 'Tempo de Exibição Total';
+SELECT SUM(length) AS 'Tempo de Exibição Total' FROM sakila.film;
+-- 5 - E finalmente, a quantidade total de filmes cadastrados na tabela sakila.film como 'Filmes Registrados'.
+SELECT COUNT(*) AS 'Filmes Registrados' FROM sakila.film;
