2caa43718f77df442438af7695654af578a32790
Hugosomers <hugosomersla@gmail.com>
2021-08-26 15:39:00 -0300

    Add conteúdo do dia

---
+++ exercises/Back-End/bloco-21-funcoes-sql-joins-e-subqueries/dia-3-stored-routines-stored-functions/Conteúdo do dia.sql
@@ -0,0 +1,220 @@
+-- STORE PROCEDURES
+-- --------------------------------------------------------------------------------------------
+
+USE banco_de_dados; -- obrigatório para criar a procedure no banco correto
+DELIMITER $$ -- definindo delimitador
+
+CREATE PROCEDURE nome_da_procedure(@parametro1, @parametro2, ..., @parametroN) -- parâmetros
+BEGIN -- delimitando o início do código SQL
+
+END $$ -- delimitando o final do código SQL
+
+DELIMITER ; -- muda o delimitador de volta para ; - o espaço entre DELIMITER e o ';' é necessário
+
+-- Procedure sem parâmetros:
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE ShowAllActors()
+BEGIN
+    SELECT * FROM sakila.actor;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+CALL ShowAllActors();
+
+-- --------------------------------------------------------------------------------------------
+
+-- Procedure com parâmetros de entrada (IN)
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE ShowActorsWithSyllable(IN syllable VARCHAR(100))
+BEGIN
+    SELECT *
+    FROM sakila.actor
+    WHERE first_name LIKE CONCAT('%', syllable, '%');
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+CALL ShowActorsWithSyllable('lope');
+
+-- --------------------------------------------------------------------------------------------
+
+-- Procedure com parâmetros de saida (OUT)
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE ShowAverageRentalDurationOfMovie(
+    IN film_name VARCHAR(300),
+    OUT media_aluguel_em_dias DOUBLE
+)
+BEGIN
+    SELECT AVG(rental_duration) INTO media_aluguel_em_dias
+    FROM sakila.film
+    WHERE title = film_name;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+CALL ShowAverageRentalDurationOfMovie('ACADEMY DINOSAUR', @media_de_dias);
+SELECT @media_de_dias;
+
+-- --------------------------------------------------------------------------------------------
+
+-- Procedure com parâmetros de entrada-saida (IN-OUT)
+
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE NameGenerator(INOUT film_name VARCHAR(300))
+BEGIN
+    SELECT CONCAT('ULTRA ', film_name, ' THE BEST MOVIE OF THE CENTURY')
+    INTO film_name;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+SELECT 'ACE GOLDFINGER' INTO @movie_title;
+CALL NameGenerator(@movie_title);
+SELECT @movie_title;
+
+-- --------------------------------------------------------------------------------------------
+
+-- STORED FUNCTIONS
+
+USE banco_de_dados; -- obrigatório para criar a função no banco correto
+DELIMITER $$
+
+CREATE FUNCTION nome_da_function(parametro1, parametro2, ..., parametroN)
+RETURNS tipo_de_dado tipo_de_retorno
+BEGIN
+    query_sql
+    RETURN resultado_a_ser_retornado;
+END $$
+
+DELIMITER ;
+
+-- --------------------------------------------------------------------------------------------
+
+-- Uma stored function que exibe a quantidade de filmes em que um determinado ator ou atriz atuou:
+USE sakila;
+DELIMITER $$
+
+CREATE FUNCTION MoviesWithActor(actor_id int)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE movie_total INT;
+    SELECT COUNT(*)
+    FROM sakila.film_actor
+    WHERE sakila.film_actor.actor_id = actor_id INTO movie_total;
+    RETURN movie_total;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+SELECT MoviesWithActor(1);
+
+-- --------------------------------------------------------------------------------------------
+
+-- Uma stored function que exibe o nome completo de um ator ou de uma atriz, dado seu id como parâmetro:
+USE sakila;
+DELIMITER $$
+
+CREATE FUNCTION GetFullName(id INT)
+RETURNS VARCHAR(200) READS SQL DATA
+BEGIN
+    DECLARE full_name VARCHAR(200);
+    SELECT concat(first_name, ' ', last_name)
+    FROM sakila.actor
+    WHERE actor_id = id
+    LIMIT 1
+    INTO full_name ;
+    RETURN full_name;
+END $$
+
+DELIMITER ;
+
+SELECT GetFullName(51);
+
+-- --------------------------------------------------------------------------------------------
+
+-- TRIGGER
+
+DELIMITER $$
+
+CREATE TRIGGER nome_do_trigger
+[BEFORE | AFTER] [INSERT | UPDATE | DELETE] ON tabela
+FOR EACH ROW
+BEGIN
+    -- o código SQL entra aqui
+END $$
+
+DELIMITER $$ ;
+
+-- --------------------------------------------------------------------------------------------
+CREATE DATABASE IF NOT EXISTS rede_social;
+
+USE rede_social;
+
+CREATE TABLE perfil(
+    perfil_id INT PRIMARY KEY auto_increment,
+    saldo DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    ultima_atualizacao DATETIME,
+    acao VARCHAR(50),
+    ativo BOOLEAN DEFAULT 1
+) engine = InnoDB;
+
+CREATE TABLE log_perfil(
+    acao_id INT PRIMARY KEY AUTO_INCREMENT,
+    acao VARCHAR(300),
+    data_acao DATE
+) engine = InnoDB;
+
+-- TRIGGER INSERT
+USE rede_social;
+
+DELIMITER $$
+CREATE TRIGGER trigger_perfil_insert
+    BEFORE INSERT ON perfil
+    FOR EACH ROW
+BEGIN
+    SET NEW.ultima_atualizacao = NOW(),
+        NEW.acao = 'INSERT';
+END $$
+DELIMITER ;
+-- --------------------------------------------------------------------------------------------
+
+-- TRIGGER UPDATE
+USE rede_social;
+
+DELIMITER $$
+CREATE TRIGGER trigger_perfil_update
+    BEFORE UPDATE ON perfil
+    FOR EACH ROW
+BEGIN
+    SET NEW.ultima_atualizacao = NOW(),
+        NEW.acao = 'UPDATE';
+END $$
+DELIMITER ;
+-- --------------------------------------------------------------------------------------------
+-- TRIGGER DELETE
+USE rede_social;
+
+DELIMITER $$
+CREATE TRIGGER trigger_perfil_delete
+    AFTER DELETE ON perfil
+    FOR EACH ROW
+BEGIN
+    INSERT INTO log_perfil(acao, data_acao)
+    VALUES ('exclusão', NOW());
+END $$
+DELIMITER ;
+-- --------------------------------------------------------------------------------------------
